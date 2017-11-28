a9726d2a8712e742ffd9780a7b14a76b3a755de9
BarbaraCortes <34046106+BarbaraCortes@users.noreply.github.com>
2017-11-28 00:04:33 -0200

    finding bridges
    Tarjan's algorithm to find bridges in a bidirectional connected graph

---
+++ tarjan.cpp
@@ -0,0 +1,31 @@
+int cnt;
+int low[N];
+int entrada[N];
+vector<int> adj[N];
+
+void dfs(int u, int p = -1) {
+	entrada[u] = ++cnt;
+	low[u] = entrada[u];
+
+	for (int i = 0; i < adj[u].size(); i++) {
+		int v = adj[u][i];
+		if (v == p) continue;
+	
+		if (entrada[v]) low[u] = min(low[u], entrada[v]);
+		else {
+      dfs(v, u);
+			low[u] = min(low[u], low[v]);
+			if (entrada[u] < low[v]) //ponte
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	
+	//for (int i = 1; i <= n; i++) {
+		//if (!entrada[i]) dfs(i);
+	//}
+  
+	return 0;
+}
