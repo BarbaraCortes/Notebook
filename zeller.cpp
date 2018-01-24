3cbc12a90b4a0e6095fc3d34d81b022ae172e90f
BarbaraCortes <34046106+BarbaraCortes@users.noreply.github.com>
2018-01-23 23:23:31 -0200

    Update zeller.cpp

--- zeller.cpp
+++ zeller.cpp
@@ -2,6 +2,10 @@
 enum {SABADO, DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA};
 
 int day_of_week(int d, int m, int y) {
+	if (m == 1 || m == 2) {
+		m += 12;
+		y--;
+	}
 	int K = y%100;
 	int J = y/100;
 	return (d + (m+1)*26/10 + K + (K)/4 + J/4 - 2*J) % 7;
